3d68ae4fee4549a5d72db8d473596bdada829792
sswynbw360 <YUSUKE@YUSUKE-3.local>
2016-10-29 13:33:58 +0900

    あああ

--- web/callback.php
+++ web/callback.php
@@ -214,14 +214,5 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'Authorization: Bearer ' . $accessToken
     ));
 $result = curl_exec($ch);
-curl_close($ch);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json; charser=UTF-8',
-    'Authorization: Bearer ' . $accessToken
-    ));
-$result = curl_exec($ch);
+
 curl_close($ch);
